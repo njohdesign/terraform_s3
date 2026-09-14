cb9edc6dbc6bba23c88c952c8fc0cfb0def26476
njohdesign <njohtah@gmail.com>
2026-09-14 07:30:51 +0100

    Correct invalid S3 bucket configuration

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "njohdesign-terraform-s3-lab-20260914-01"
+  bucket = "njohdesign-terraform-s3-lab-20260914-7842"
 
   tags = {
     Name        = "Terraform S3 Lab"
