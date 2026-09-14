de3485e237fdabd214e6a7ec7a888c9fdd16512d
njohdesign <njohtah@gmail.com>
2026-09-14 07:27:54 +0100

    Fix S3 bucket name and remove deprecated ACL

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "njohdesign-terraform-s3-lab-20260914" # Replace with a unique bucket name
-  acl    = "private"
+  bucket = "njohdesign-terraform-s3-lab-20260914-01"
 
   tags = {
+    Name        = "Terraform S3 Lab"
     Environment = "Lab"
   }
 }
