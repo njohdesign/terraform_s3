197b5f4d28ad64cabcd0d57bae34232db21bb29d
njohdesign <46420553+njohdesign@users.noreply.github.com>
2026-09-14 08:07:04 +0100

    Refactor S3 bucket configuration with account ID
    Updated S3 bucket name to use account ID and added tags.

--- main.tf
+++ main.tf
@@ -2,11 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "njohdesign-terraform-s3-lab-20260914-7842"
-  acl    = "private"
+  bucket = "njohdesign-terraform-s3-${data.aws_caller_identity.current.account_id}"
 
   tags = {
+    Name        = "Terraform S3 Lab"
     Environment = "Lab"
   }
 }
